20aa89bf6ded87b3f0a6cc855ffe1a5b8cb35273
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2023-07-03 12:48:39 -0700

    wow class picker works

--- BlazorWasmTester/BlazorWasmTester/Model/WowClassPickerModel.cs
+++ BlazorWasmTester/BlazorWasmTester/Model/WowClassPickerModel.cs
@@ -27,6 +27,10 @@
             new Race() { Name = RaceNames.Tauren},
             new Race() { Name = RaceNames.Troll},
             new Race() { Name = RaceNames.Undead},
+            new Race() { Name = RaceNames.Human},
+            new Race() { Name = RaceNames.Dwarf},
+            new Race() { Name = RaceNames.Gnome},
+            new Race() { Name = RaceNames.NightElf},
         };
         public static List<Class> Classes { get; set; } = new List<Class>
         {
@@ -42,6 +46,109 @@
         };
 
 
+        public static List<(ClassNames ClassName, RaceNames RaceName)> xx = new List<(ClassNames ClassName, RaceNames RaceName)>
+        {
+            (ClassNames.Warrior, RaceNames.Orc),
+            (ClassNames.Warrior, RaceNames.Troll),
+            (ClassNames.Warrior, RaceNames.Undead),
+            (ClassNames.Warrior, RaceNames.Tauren),
+            (ClassNames.Warrior, RaceNames.Human),
+            (ClassNames.Warrior, RaceNames.Dwarf),
+            (ClassNames.Warrior, RaceNames.Gnome),
+            (ClassNames.Warrior, RaceNames.NightElf),
+
+            (ClassNames.Paladin, RaceNames.Human),
+            (ClassNames.Paladin, RaceNames.Dwarf),
+
+            (ClassNames.Hunter, RaceNames.Orc),
+            (ClassNames.Hunter, RaceNames.Troll),
+            (ClassNames.Hunter, RaceNames.Tauren),
+            (ClassNames.Hunter, RaceNames.Dwarf),
+            (ClassNames.Hunter, RaceNames.NightElf),
+                    
+            (ClassNames.Shaman, RaceNames.Orc),
+            (ClassNames.Shaman, RaceNames.Troll),
+            (ClassNames.Shaman, RaceNames.Tauren),
+
+            (ClassNames.Druid, RaceNames.Tauren),
+            (ClassNames.Druid, RaceNames.NightElf),
+
+            (ClassNames.Rogue, RaceNames.Orc),
+            (ClassNames.Rogue, RaceNames.Troll),
+            (ClassNames.Rogue, RaceNames.Undead),
+            (ClassNames.Rogue, RaceNames.Human),
+            (ClassNames.Rogue, RaceNames.Dwarf),
+            (ClassNames.Rogue, RaceNames.Gnome),
+            (ClassNames.Rogue, RaceNames.NightElf),
+
+            (ClassNames.Mage, RaceNames.Troll),
+            (ClassNames.Mage, RaceNames.Undead),
+            (ClassNames.Mage, RaceNames.Human),
+            (ClassNames.Mage, RaceNames.Gnome),
+
+            (ClassNames.Warlock, RaceNames.Orc),
+            (ClassNames.Warlock, RaceNames.Undead),
+            (ClassNames.Warlock, RaceNames.Human),
+            (ClassNames.Warlock, RaceNames.Gnome),
+
+            (ClassNames.Priest, RaceNames.Troll),
+            (ClassNames.Priest, RaceNames.Undead),
+            (ClassNames.Priest, RaceNames.Human),
+            (ClassNames.Priest, RaceNames.Dwarf),
+            (ClassNames.Priest, RaceNames.NightElf),
+        };
+        //public static Dictionary<ClassNames, RaceNames> ClassRaceCombinations { get; set; } =
+        //    new Dictionary<ClassNames, RaceNames>
+        //    {
+        //        { ClassNames.Warrior,RaceNames.Orc },
+        //        { ClassNames.Warrior,RaceNames.Troll },
+        //        { ClassNames.Warrior,RaceNames.Undead },
+        //        { ClassNames.Warrior,RaceNames.Tauren },
+        //        { ClassNames.Warrior,RaceNames.Human },
+        //        { ClassNames.Warrior,RaceNames.Dwarf },
+        //        { ClassNames.Warrior,RaceNames.Gnome },
+        //        { ClassNames.Warrior,RaceNames.NightElf },
+
+        //        { ClassNames.Paladin,RaceNames.Human },
+        //        { ClassNames.Paladin,RaceNames.Dwarf },
+
+        //        { ClassNames.Hunter,RaceNames.Orc },
+        //        { ClassNames.Hunter,RaceNames.Troll },
+        //        { ClassNames.Hunter,RaceNames.Tauren },
+        //        { ClassNames.Hunter,RaceNames.Dwarf },
+        //        { ClassNames.Hunter,RaceNames.NightElf },
+
+        //        { ClassNames.Shaman,RaceNames.Orc },
+        //        { ClassNames.Shaman,RaceNames.Troll },
+        //        { ClassNames.Shaman,RaceNames.Tauren },
+
+        //        { ClassNames.Druid,RaceNames.Tauren },
+        //        { ClassNames.Druid,RaceNames.NightElf },
+
+        //        { ClassNames.Rogue,RaceNames.Orc },
+        //        { ClassNames.Rogue,RaceNames.Troll},
+        //        { ClassNames.Rogue,RaceNames.Undead },
+        //        { ClassNames.Rogue,RaceNames.Human },
+        //        { ClassNames.Rogue,RaceNames.Dwarf },
+        //        { ClassNames.Rogue,RaceNames.Gnome},
+        //        { ClassNames.Rogue,RaceNames.NightElf },
+
+        //        { ClassNames.Mage,RaceNames.Troll },
+        //        { ClassNames.Mage,RaceNames.Undead },
+        //        { ClassNames.Mage,RaceNames.Human },
+        //        { ClassNames.Mage,RaceNames.Gnome },
+
+        //        { ClassNames.Warlock,RaceNames.Orc },
+        //        { ClassNames.Warlock,RaceNames.Undead },
+        //        { ClassNames.Warlock,RaceNames.Human },
+        //        { ClassNames.Warlock,RaceNames.Gnome },
+
+        //        { ClassNames.Priest,RaceNames.Troll },
+        //        { ClassNames.Priest,RaceNames.Undead },
+        //        { ClassNames.Priest,RaceNames.Human },
+        //        { ClassNames.Priest,RaceNames.Dwarf },
+        //        { ClassNames.Priest,RaceNames.NightElf },
+        //    };
 
 
     }
