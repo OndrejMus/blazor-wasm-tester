4d7d2284b903dd7d7c0971d20a733a537e70cb4e
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2023-07-04 15:40:42 -0700

    name generator works but needs tuning

---
+++ BlazorWasmTester/BlazorWasmTester/ChatGptIntegration.cs
@@ -0,0 +1,40 @@
+﻿using OpenAI_API;
+
+namespace BlazorWasmTester
+{
+    public class ChatGptIntegration
+    {
+        private OpenAIAPI api;
+
+        public ChatGptIntegration(string apiKey)
+        {
+            api = new OpenAIAPI(apiKey);
+        }
+
+        public async Task<string> SimpleQueryWithConditioning(string question, string systemMessage = null,
+            List<(string exampleQuestion, string exampleResponse)> conditioning = null)
+        {
+            var chat = api.Chat.CreateConversation();
+            /// give instruction as System
+            chat.AppendSystemMessage(systemMessage);
+
+            if (conditioning != null)
+            {
+                foreach (var condition in conditioning) 
+                {
+                    // give a few examples as user and assistant
+                    chat.AppendUserInput(condition.exampleQuestion);
+                    chat.AppendExampleChatbotOutput(condition.exampleResponse);
+                }
+            }            
+
+            // now let's ask it a question'
+            chat.AppendUserInput(question);
+            // and get the response
+            string response = await chat.GetResponseFromChatbotAsync();
+
+            return response;
+        }
+
+    }
+}
